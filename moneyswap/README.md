45ebce3e8fb409be97199417831f862fbef6b002
Hrishikesh Bhat <hrishibhat@gmail.com>
2022-06-02 15:20:54 +0530

    project:Added Moneyswap project

---
+++ moneyswap/README.md
@@ -0,0 +1,15 @@
+# Pragramming Uniswap V1
+
+This is a self-learning project to program Uniswap V1 from scratch. 
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat accounts
+npx hardhat compile
+npx hardhat clean
+npx hardhat test
+npx hardhat node
+node scripts/sample-script.js
+npx hardhat help
+```
---
+++ moneyswap/contracts/Exchange.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract Exchange {
+    address public tokenAddress;
+
+    constructor(address _tokenAddress) {
+        require(_tokenAddress != address(0), "Invalid token address");
+
+        tokenAddress = _tokenAddress;
+    }
+
+    function addLiquidity(uint256 _tokenAmount) public payable {
+        IERC20 token = IERC20(tokenAddress);
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
+
+    function getReserve() public view returns (uint256) {
+        return IERC20(tokenAddress).balanceOf(address(this));
+    }
+
+    function getPrice(uint256 inputReserve, uint256 outputReserve)
+        public
+        pure
+        returns (uint256)
+    {
+        require(inputReserve > 0 && outputReserve > 0, "invalid reserves");
+
+        return inputReserve / outputReserve;
+    }
+}
